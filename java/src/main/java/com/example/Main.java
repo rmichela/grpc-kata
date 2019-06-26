9f40a99916fbc809e2b4cd06647a3601b9032745
Ryan Michela <rmichela@salesforce.com>
2019-06-26 15:29:25 -0400

    Add GreeterImpl class

--- java/src/main/java/com/example/Main.java
+++ java/src/main/java/com/example/Main.java
@@ -1,6 +1,22 @@
 package com.example;
 
+import io.grpc.stub.*;
+
 public class Main {
+    public static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(GreeterOuterClass.HelloRequest request, StreamObserver<GreeterOuterClass.HelloResponse> responseObserver) {
+            System.out.println("Java service request: " + request.getName());
+
+            responseObserver.onNext(GreeterOuterClass.HelloResponse.newBuilder()
+                .addMessage("Hello " + request.getName())
+                .addMessage("Aloha " + request.getName())
+                .addMessage("Howdy " + request.getName())
+                .build());
+            responseObserver.onCompleted();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Java");
     }
