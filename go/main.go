091fc30b8948b2403f4f7f9de6a15cffc3592396
Ryan Michela <rmichela@salesforce.com>
2019-06-27 11:34:18 -0400

    Implement greeter service

--- go/main.go
+++ go/main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 )
+
+type greeter struct{}
+
+func (g *greeter) SayHello(ctx context.Context, req *HelloRequest) (*HelloResponse, error) {
+	fmt.Println("Go service request: " + req.GetName())
+	resp := HelloResponse{
+		Message: []string{
+			"Hello " + req.GetName(),
+			"Aloha " + req.GetName(),
+			"Howdy " + req.GetName()},
+	}
+	return &resp, nil
+}
 
 func main() {
 	fmt.Println("Hello Golang")
