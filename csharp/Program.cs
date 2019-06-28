f7d17089c7698e5d429b1a033307780f8acaefb2
Ryan Michela <rmichela@salesforce.com>
2019-06-28 11:26:36 -0400

    Implement greeter service

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Com.Example;
+using Grpc.Core;
 
 namespace example_csharp
 {
+    class GreeterImpl : Greeter.GreeterBase
+    {
+        public override Task<HelloResponse> SayHello(HelloRequest request, ServerCallContext context) {
+            Console.WriteLine("Java service request: " + request.Name);
+            return Task.FromResult(new HelloResponse {
+                Message = {
+                    "Hello " + request.Name,
+                    "Aloha " + request.Name,
+                    "Howdy " + request.Name 
+                }
+            });
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
